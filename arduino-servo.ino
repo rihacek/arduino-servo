365659175000356cafb38c58e8827a565d25ba2e
John Rihacek <jrihacek@gmail.com>
2022-05-09 19:12:11 -0400

    hook up servo to make sure it works

--- arduino-servo.ino
+++ arduino-servo.ino
@@ -1,15 +1,39 @@
-void setup() {
+#include <Servo.h> 
 
-  // initialize digital pin LED_BUILTIN as an output.
+int servoPin = 5;
+int servoPos = 30; // set the limit of servo position 
+ 
+Servo servo; 
+
+int angle = 0;
+
+void setup() {
+  servo.attach(servoPin);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-// the loop function runs over and over again forever
+void servoMove()
+{ 
+  // scan from 0 to servoPos degrees
+  for(angle = 0; angle < servoPos; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(15);                   
+  } 
+  // now scan back from servoPos to 0 degrees
+  for(angle = servoPos; angle > 0; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(15);
+  }       
+}
+
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // Arduino: turn the LED on (HIGH)
-                                     // D1 Mini: turns the LED *off*
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // Arduino: turn the LED off (LOW)
-                                     // D1 Mini: turns the LED *on*
-  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(2000);
+  
+  servoMove();
+
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000);
 }
